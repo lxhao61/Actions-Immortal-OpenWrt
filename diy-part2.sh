307bf4227732cc59fc34c1dbe3ea4ec3787b63a1
lxhao61 <lxhao61@gmail.com>
2021-09-19 06:11:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # 取消Immortalwrt大登陆密码
-#sed -i 's/^\(.*99999\)/#&/' package/lean/default-settings/files/zzz-default-settings
+sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/zzz-default-settings

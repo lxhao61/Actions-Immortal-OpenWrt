1b27ee648e3cdc52e847df24929f8bac7c9dbbf1
lxhao61 <lxhao61@gmail.com>
2021-11-30 04:52:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
-# 取消Immortalwrt大登陆密码
+# 取消Immortal大登陆密码
 sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings
 
 # 删除luci-app-passwall插件

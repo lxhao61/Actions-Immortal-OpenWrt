67d87aec42e6cca22b8ac5e76978e371b4360053
lxhao61 <lxhao61@gmail.com>
2021-12-18 02:19:28 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,6 +16,12 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 取消Immortal大登陆密码
 sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings
 
+# 删除原版softethervpn5插件
+rm -rf feeds/packages/net/softethervpn5
+
+# 拉取Lean大softethervpn5插件
+svn co https://github.com/coolsnowwolf/packages/trunk/net/softethervpn5 feeds/packages/net/softethervpn5
+
 # 删除luci-app-passwall插件
 #rm -rf package/feeds/packages/luci-app-passwall
 

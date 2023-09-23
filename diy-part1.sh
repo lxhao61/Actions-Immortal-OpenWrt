71d99ab4f5123d6b57e97856bf28009fc4f4e82c
lxhao61 <lxhao61@gmail.com>
2023-09-23 11:48:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,3 +19,6 @@
 
 # 添加xiaorouji大passwall源
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+
+# 添加phtunnel、pgyvpn源码
+echo 'src-git OpenOray https://github.com/OrayOS/OpenOray' >>feeds.conf.default

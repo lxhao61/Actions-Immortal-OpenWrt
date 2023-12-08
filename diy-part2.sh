44054bdc2ac36df7e08630e1872a6b533f8ea5e8
lxhao61 <lxhao61@gmail.com>
2023-12-09 00:34:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 取消Immortal大登陆密码
 sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings

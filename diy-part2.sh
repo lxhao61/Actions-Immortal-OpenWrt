0c24fca753fc6a26c95a2742be2617a80dfa21fe
lxhao61 <lxhao61@gmail.com>
2024-01-09 08:11:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,4 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings
 
 # 拉取 lucky、luci-app-lucky 源码
-git clone https://github.com/gdy666/luci-app-lucky.git package/lucky/luci-app-lucky
-
-# 拉取 phtunnel、pgyvpn 源码
-#git clone https://github.com/OrayOS/OpenOray.git package/feeds/OpenOray
-
-# 提取 phtunnel 源码
-#svn co https://github.com/coolsnowwolf/packages/trunk/net/phtunnel package/feeds/packages/phtunnel
-
-# 提取 luci-app-phtunnel 源码
-#svn co https://github.com/OrayOS/OpenOray/trunk/luci-app-phtunnel package/feeds/luci/luci-app-phtunnel
+#git clone https://github.com/gdy666/luci-app-lucky.git package/lucky/luci-app-lucky

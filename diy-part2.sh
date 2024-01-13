43d398db1357d5bad8b2d048d355b3d9379648e9
lxhao61 <lxhao61@gmail.com>
2024-01-13 20:48:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,11 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
+# 修改主机名
+sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
+
 # 取消Immortal大登陆密码
-sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings
+#sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings
 
 # 拉取 lucky、luci-app-lucky 源码
 #git clone https://github.com/gdy666/luci-app-lucky.git package/lucky/luci-app-lucky

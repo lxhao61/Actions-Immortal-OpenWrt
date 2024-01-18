a9cd9085bff7beb298f0f8aa2bb913bfd1e8da3c
lxhao61 <lxhao61@gmail.com>
2024-01-18 10:21:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,7 @@
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # 取消Immortal大登陆密码
-sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings
+#sed -i 's/^\(.*99999\)/#&/' package/emortal/default-settings/files/99-default-settings
 
 # 拉取phtunnel、pgyvpn源码
 #git clone https://github.com/OrayOS/OpenOray.git package/OpenOray

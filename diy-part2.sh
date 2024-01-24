196cc29541983e5985cf110fe7f6f6b47dd8f1f5
lxhao61 <lxhao61@gmail.com>
2024-01-25 05:33:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,47 +17,27 @@ sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generat
 # 修改主机名
 sed -i "s/hostname='.*'/hostname='OpenWrt'/g" package/base-files/files/bin/config_generate
 
-# 删除自带 golang
-#rm -rf feeds/packages/lang/golang
-
-# 拉取 golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang
-
-# 删除自带 hysteria
-#rm -rf feeds/packages/net/hysteria
-#rm -rf package/feeds/packages/hysteria
-
-# 删除自带 gn
-#rm -rf feeds/packages/devel/gn
-#rm -rf package/feeds/packages/gn
-
-# 删除自带 naiveproxy
-#rm -rf feeds/packages/net/naiveproxy
-#rm -rf package/feeds/packages/naiveproxy
-
-# 删除自带 v2ray-geodata
-#rm -rf feeds/packages/net/v2ray-geodata
-#rm -rf package/feeds/packages/v2ray-geodata
-
 # 删除自带 xray-core
-#rm -rf feeds/packages/net/xray-core
-#rm -rf package/feeds/packages/xray-core
+rm -rf feeds/packages/net/xray-core
 
-# 删除自带 luci-app-passwall
-#rm -rf feeds/luci/applications/luci-app-passwall
-#rm -rf package/feeds/luci/luci-app-passwall
-
-# 拉取 passwall-packages
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/passwall/packages
-#cd package/passwall/packages
-#git checkout c189a68728d6bb65d9fb4b47fdacea3ba970a624
-#cd -
-
-# 拉取 luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall/luci-app-passwall
-#cd package/passwall/luci-app-passwall
-#git checkout d1e618220a9a0a4b73d536101f452a2f4cf14861
-#cd -
-
-# 拉取 ShadowSocksR Plus+
-#git clone -b master https://github.com/fw876/helloworld.git package/helloworld
+# 筛选程序
+function merge_package(){
+    # 参数1是分支名,参数2是库地址。所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
+# 提取 xray-core
+merge_package master https://github.com/immortalwrt/packages.git feeds/packages/net/xray-core net/xray-core

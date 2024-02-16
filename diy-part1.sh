9d6eefc9b57eecb07831f162ca06e7a2ae0b9d6c
lxhao61 <lxhao61@gmail.com>
2024-02-16 16:40:15 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,10 @@ git checkout v23.05.1
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# 注释默认 packages
+sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
+# 添加回退 packages
+sed -i '$a src-git packages https://github.com/immortalwrt/packages.git^1820d9b' feeds.conf.default
